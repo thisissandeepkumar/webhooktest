63bf2c2dfc38643938bac6f402b890fea77d1080
Sandeep Kumar <sandeepmahendran@gmail.com>
2022-09-26 13:28:12 +0530

    Initial

--- app.js
+++ app.js
@@ -3,7 +3,13 @@ const app = Express();
 
 app.use(Express.json());
 
-app.post("/webhook", async (req, res, next) => {
+app.post("/webhook/search", async (req, res, next) => {
+  console.log(req.body);
+  res.status(200).json({
+    message: "OK",
+  });
+});
+app.post("/webhook/on_search", async (req, res, next) => {
   console.log(req.body);
   res.status(200).json({
     message: "OK",
